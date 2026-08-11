2586e30a8272e93369b8a66924f2e4900728e28b
SuperTaeyun <super.kim.taeyun@gmail.com>
2026-08-11 11:42:18 +0900

    chore(ci): 테스트 비활성화, CI 동작 설정 정상화
    해당 테스트를 비활성화하고 chore/github-actions-ci를 대상으로한 push에 actions가 동작하지 않도록 한다.
    
    #4

--- backend/src/test/java/com/taeyun/flashsale/CiVerificationTest.java
+++ backend/src/test/java/com/taeyun/flashsale/CiVerificationTest.java
@@ -11,7 +11,7 @@ class CiVerificationTest {
      * 반드시 실패하는 테스트. 필요한 순간을 제외하면 절대로. 반드시 실행되어서는 안된다.
      */
     @Test
-//    @Disabled
+    @Disabled
     void intentionallyFailingTest() {
         fail("This test is intentionally failing");
     }
