878004dbca203b696124dfab9638b17b12f0effd
SuperTaeyun <super.kim.taeyun@gmail.com>
2026-08-21 13:20:08 +0900

    feat: BaseEntity 추가
    거의 모든 엔티티 클래스가 공유할 BaseEntity 클래스를 추가한다.
    
    #10

---
+++ backend/src/main/java/com/taeyun/flashsale/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.taeyun.flashsale.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
